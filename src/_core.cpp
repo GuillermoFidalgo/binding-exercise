760f5f52cf35ff1b1439d3d84a07ecf8b98d5d0a
Guillermo Fidalgo <guillermo.fidalgo@upr.edu>
2024-04-20 14:34:29 -0400

    add binding and finish cmake config

--- src/_core.cpp
+++ src/_core.cpp
@@ -8,4 +8,7 @@ namespace py = pybind11;
 
 PYBIND11_MODULE(_core, m) {
   /// ... Fill me in!
+  m.def("HarmonicOscillator", &HarmonicOscillator, R"pbdoc(
+    A harmonico oscillator class
+    )pbdoc");
 }
